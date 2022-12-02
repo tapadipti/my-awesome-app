fb51bb450b60d55b69df548974272e40acaf2a7b
tapadipti <tapadipti@gmail.com>
2022-12-02 18:03:54 +0545

    Added model registry dummy links

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -27,6 +27,16 @@ export default function AllPosts() {
 
   return (
     <div>
+      <h2>Model Registry</h2>
+      <h3>Add model</h3>
+      <h3>Register version</h3>
+      <h3>Assign stage</h3>
+      <h3>View model details</h3>
+      <h3>Etc...</h3>
+      <h3>Etc...</h3>
+
+
+
       <h2>Blog Posts</h2>
       <h3>Welcome to my blog posts page!</h3>
       <div>
